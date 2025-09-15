9387eb25eb1f6682158769dcf9641f57a7b2f18a
Avinash <avinashdola57@gmail.com>
2025-09-15 18:06:11 +0530

    added

--- LibraryManagement/src/main/java/com/klef/LibraryManagementApplication.java
+++ LibraryManagement/src/main/java/com/klef/LibraryManagementApplication.java
@@ -1,10 +1,11 @@
 package com.klef;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementApplication {
+public class LibraryManagementApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
